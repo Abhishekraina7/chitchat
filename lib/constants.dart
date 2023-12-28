d563b78b8b08cec23eb2e21030ce41b019fd4960
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-12-28 22:10:27 +0530

    Created a universal background color for all the screens.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const kBackGroundColor = Colors.black;
+
 const kSendButtonTextStyle = TextStyle(
   color: Colors.lightBlueAccent,
   fontWeight: FontWeight.bold,
