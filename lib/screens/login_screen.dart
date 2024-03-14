4ac138127cf596a9903792751414171d882925a0
Abhishek Raina <abhishek.raina.uit@gmail.com>
2024-03-14 12:50:08 +0530

    Imported the buttons.dart file and used the button object to make our code more modular.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:chitchat/constants.dart';
+import 'package:chitchat/components/buttons.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'loginScreen';
+
   const LoginScreen({super.key});
   @override
   LoginScreenState createState() => LoginScreenState();
@@ -32,7 +34,7 @@ class LoginScreenState extends State<LoginScreen> {
               child: Hero(
                 tag: 'logo',
                 child: Container(
-                  height: 300.0,
+                  height: 170.0,
                   child: Image.asset('images/loginscreenback.png'),
                 ),
               ),
@@ -92,27 +94,27 @@ class LoginScreenState extends State<LoginScreen> {
             const SizedBox(
               height: 24.0,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: const Color(0xFF95FF80),
-                borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Implement login functionality.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
+            Buttons(color: const Color(0xFF95FF80), name: 'Log in', onPressed: (){},),
+
           ],
         ),
       ),
     );
   }
 }
+// Padding(
+// padding: const EdgeInsets.symmetric(vertical: 16.0),
+// child: Material(
+// color: const Color(0xFF95FF80),
+// borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+// elevation: 5.0,
+// child: MaterialButton(
+// onPressed: () {
+// //Implement login functionality.
+// },
+// minWidth: 200.0,
+// height: 35.0,
+// child:  const SingleChildScrollView(child: Text('Log In',)),
+// ),
+// ),
+// ),
