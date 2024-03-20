c7c743a2bb6b93b34e2d21fd466f64de9d8d17d4
Abhishek Raina <abhishek.raina.uit@gmail.com>
2024-03-20 20:43:24 +0530

    made some minor changes in name of App and text color.

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,20 @@ import 'package:chitchat/screens/login_screen.dart';
 import 'package:chitchat/screens/registration_screen.dart';
 import 'package:chitchat/screens/chat_screen.dart';
 
-void main() => runApp(const FlashChat());
+void main() => runApp(const ChitChat());
 
-class FlashChat extends StatelessWidget {
-  const FlashChat({super.key});
+class ChitChat extends StatelessWidget {
+  const ChitChat({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         textTheme: const TextTheme(
-          bodyLarge: TextStyle(color: Colors.black54),
+          bodyLarge: TextStyle(color: Colors.white),
         ),
       ),
       // home: const WelcomeScreen(), // anyscreen in home open first when the app is started
-      initialRoute: WelcomeScreen.id, // similar to home.
+      initialRoute: ChatScreen.id, // similar to home.
       routes: {
         ChatScreen.id:(context) =>  const ChatScreen(),
         LoginScreen.id:(context) =>  const LoginScreen(),
